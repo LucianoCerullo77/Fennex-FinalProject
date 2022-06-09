72f741a6f5aaec246093b1e44c5a5b021b0f673b
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-06-09 15:03:50 -0300

    se instalo firebase

--- src/index.js
+++ src/index.js
@@ -5,6 +5,28 @@ import reportWebVitals from './reportWebVitals';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDpMpFuK2O_B0BW7zC7AQ8sVOOzC6LQbLs",
+  authDomain: "fennex-ec.firebaseapp.com",
+  projectId: "fennex-ec",
+  storageBucket: "fennex-ec.appspot.com",
+  messagingSenderId: "132932748897",
+  appId: "1:132932748897:web:4d7054e3981d230ff58c20",
+  measurementId: "G-WPR6SCDGVK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 import "./styles/styles.sass";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
